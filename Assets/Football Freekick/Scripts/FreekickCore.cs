bb6025079cd1078b54729e5ff4621d708c8dacb8
Bilge-Han <bilgeacar095@gmail.com>
2024-05-02 13:42:07 +0300

    .

---
+++ Assets/Football Freekick/Scripts/FreekickCore.cs
@@ -0,0 +1,247 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FreekickCore : MonoBehaviour
+{
+    [Header("Ball and Football Arena")]
+    public Rigidbody ball;
+    public AudioSource shootAudioSource;
+    public AudioClip shootAudioClip;
+    public GameObject footballArena;
+    
+    [Header("UI")]
+    public RectTransform ballHitPos;
+    public Slider shootSlider;
+    public TrajectoryLineRenderer trajectoryLineRenderer;
+    public GameObject ballHitCanvas, directionCanvas, freekickPositionSelectionPanel, freekickSettingPanel, freekickCompletedPanel;
+    public TextMeshProUGUI ballDistanceText;
+
+    [Header("Points")] 
+    public Transform freekickPoint;
+    public Transform farTarget, nearTarget, goalCenter;
+    
+    private float _power;
+    private const float XMin = -34.8f, XMax = 34.8f, ZMin = -103f, ZMax = 0, BallHitPosRadius = 0.435f;
+    private Vector3 _farTargetLocalPos, _nearTargetLocalPos;
+    private bool _isUnavailableForNewFreekick, _isShooting, _isBallHit, _isSetShootSlider;
+    private bool _isRightKickAngle, _isLeftKickAngle;
+    private bool _isForwardFreekickPosition, _isBackFreekickPosition, _isRightFreekickPosition, _isLeftFreekickPosition;
+    private bool _isUpBallHitPos, _isDownBallHitPos, _isRightBallHitPos, _isLeftBallHitPos;
+
+    private void Awake()
+    {
+        _farTargetLocalPos = farTarget.localPosition;
+        _nearTargetLocalPos = nearTarget.localPosition;
+    }
+
+    private void Update()
+    {
+        // Opens and closes "Football Arena" gameObject if pressing "F" button.
+        if(Input.GetKeyDown(KeyCode.F)) footballArena.SetActive(!footballArena.activeSelf);
+        
+        // When available creates new freekick if pressing "Enter" button.
+        if (!_isUnavailableForNewFreekick && Input.GetKeyDown(KeyCode.Return)) NewFreekickCreate();
+        
+        // Set Kick Angles.
+        _isRightKickAngle = !_isBallHit && Input.GetKey(KeyCode.RightArrow);
+        _isLeftKickAngle = !_isBallHit && Input.GetKey(KeyCode.LeftArrow);
+        
+        // Set Freekick Position.
+        _isForwardFreekickPosition = !_isShooting && Input.GetKey(KeyCode.W);
+        _isBackFreekickPosition = !_isShooting && Input.GetKey(KeyCode.S);
+        _isRightFreekickPosition = !_isShooting && Input.GetKey(KeyCode.D);
+        _isLeftFreekickPosition = !_isShooting && Input.GetKey(KeyCode.A);
+
+        if (_isShooting)
+        {
+            if (directionCanvas.activeSelf) directionCanvas.SetActive(false);
+            if (!_isBallHit)
+            {
+                // Applying UI changes for shooting.
+                if (!ballHitCanvas.activeSelf) ballHitCanvas.SetActive(true);
+                if (!trajectoryLineRenderer.gameObject.activeSelf) trajectoryLineRenderer.gameObject.SetActive(true);
+                SetInstructionPanels(false, true, false);
+
+                // Set New Freekick Shoot Ball Hit Pos (curve).
+                _isUpBallHitPos = Input.GetKey(KeyCode.W);
+                _isDownBallHitPos = Input.GetKey(KeyCode.S);
+                _isRightBallHitPos = Input.GetKey(KeyCode.D);
+                _isLeftBallHitPos = Input.GetKey(KeyCode.A);
+
+                var originPoint = Vector3.zero;
+                var ballHitPosLocal = ballHitPos.localPosition;
+                var distance = Vector3.Distance(ballHitPosLocal, originPoint);
+                var fromOriginToObject = ballHitPosLocal - originPoint;
+                
+                if (distance > BallHitPosRadius)
+                {
+                    fromOriginToObject *= BallHitPosRadius / distance;
+                    ballHitPos.localPosition = originPoint + fromOriginToObject;
+                }
+
+                // Set Freekick Curve Settings.
+                ballHitPosLocal = ballHitPos.localPosition;
+                farTarget.localPosition = new Vector3(_farTargetLocalPos.x - ballHitPosLocal.x * 20f, _farTargetLocalPos.y - ballHitPosLocal.y * 5f, _farTargetLocalPos.z);
+                nearTarget.localPosition = new Vector3(_nearTargetLocalPos.x - ballHitPosLocal.x * 0.5f, _nearTargetLocalPos.y - ballHitPosLocal.y * 0.5f, _nearTargetLocalPos.z);
+                trajectoryLineRenderer.CreateTrajectoryLine(ball.transform, nearTarget, farTarget);
+                trajectoryLineRenderer.transform.position = Vector3.zero;
+
+                // Set slider and shoot freekick.
+                _isSetShootSlider = Input.GetKey(KeyCode.Space);
+
+                // Apply force to ball for shooting.
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    ShootToTargets();
+                    StartCoroutine(WaitForResetShootSlider());
+                }
+            }
+            else
+            {
+                // Set Trajectory lines.
+                if (ballHitCanvas.activeSelf) ballHitCanvas.SetActive(false);
+                if (trajectoryLineRenderer.gameObject.activeSelf) trajectoryLineRenderer.gameObject.SetActive(false);
+            }
+        }
+        else SetNewFreekickUI();
+    }
+
+    private void FixedUpdate()
+    {
+        // Applying Kick Angles Input.
+        if(_isRightKickAngle) SetKickAngle(1);
+        if(_isLeftKickAngle) SetKickAngle(-1);
+        
+        // Applying Freekick Position Input.
+        if(_isForwardFreekickPosition) TranslateFreekickPointWithInput(Vector3.forward);
+        if(_isBackFreekickPosition) TranslateFreekickPointWithInput(-Vector3.forward);
+        if(_isRightFreekickPosition) TranslateFreekickPointWithInput(Vector3.right);
+        if(_isLeftFreekickPosition) TranslateFreekickPointWithInput(-Vector3.right);
+
+        // Applying Freekick Ball Hit Pos Input.
+        if (_isUpBallHitPos || _isDownBallHitPos || _isRightBallHitPos || _isLeftBallHitPos)
+        {
+            var ballHitDiff = ballHitCanvas.transform.localScale.magnitude;
+            if (_isUpBallHitPos) TranslateBallHitPosWithInput(Vector3.up, ballHitDiff);
+            if(_isDownBallHitPos) TranslateBallHitPosWithInput(-Vector3.up, ballHitDiff);
+            if(_isRightBallHitPos) TranslateBallHitPosWithInput(Vector3.right, ballHitDiff);
+            if(_isLeftBallHitPos) TranslateBallHitPosWithInput(-Vector3.right, ballHitDiff);
+        }
+        
+        // Applying Shoot Slider Input.
+        if(_isSetShootSlider) SetShootSlider();
+    }
+
+    private void NewFreekickCreate()
+    {
+        // When creating new freekick, all scene components and booleans are setting here.
+        if (trajectoryLineRenderer.gameObject.activeSelf) trajectoryLineRenderer.gameObject.SetActive(false);
+        _isBallHit = false;
+        _isShooting = !_isShooting;
+        ball.Sleep();
+        var ballTransform = ball.transform;
+        ballTransform.localPosition = Vector3.zero;
+        ballTransform.localEulerAngles = Vector3.zero;
+    }
+
+    private void SetNewFreekickUI()
+    {
+        // Sets all UI components for new freekick.
+        if (!directionCanvas.activeSelf) directionCanvas.SetActive(true);
+        if (ballHitCanvas.activeSelf) ballHitCanvas.SetActive(false);
+        if (!ballDistanceText.gameObject.activeSelf) ballDistanceText.gameObject.SetActive(true);
+        
+        ballDistanceText.text = DistanceConversion.Distance((ball.position - goalCenter.position).magnitude);
+        SetInstructionPanels(true, false, false);
+    }
+
+    private void TranslateFreekickPointWithInput(Vector3 direction)
+    {
+        freekickPoint.Translate(direction * (12f * Time.fixedDeltaTime));
+        
+        // restricting the ball in an area.
+        if (freekickPoint.transform.localPosition.x > XMax || freekickPoint.transform.localPosition.x < XMin ||
+            freekickPoint.transform.localPosition.z > ZMax || freekickPoint.transform.localPosition.z < ZMin)
+        {
+            freekickPoint.Translate(-direction * (12f * Time.fixedDeltaTime));
+        }
+    }
+    
+    private void TranslateBallHitPosWithInput(Vector3 direction, float ballHitDiff)
+    {
+        // Sets Ball Hit Position.
+        ballHitPos.Translate(direction * (ballHitDiff * 0.2f * Time.fixedDeltaTime));
+    }
+    
+    private void SetKickAngle(int direction)
+    {
+        // Rotates the freekick point with kick angles.
+        freekickPoint.transform.Rotate(freekickPoint.up, direction * 0.6f);
+        trajectoryLineRenderer.transform.eulerAngles = Vector3.zero;
+    }
+
+    private void SetInstructionPanels(bool positionSelection, bool setting, bool completed)
+    {
+        // Set UI visibilities.
+        if (freekickPositionSelectionPanel.activeSelf != positionSelection) freekickPositionSelectionPanel.SetActive(positionSelection);
+        if (freekickSettingPanel.activeSelf != setting) freekickSettingPanel.SetActive(setting);
+        if (freekickCompletedPanel.activeSelf != completed) freekickCompletedPanel.SetActive(completed);
+    }
+
+    private void SetShootSlider()
+    {
+        // Set shoot slider if player presses "Space" key.
+        if (_power >= 30f) return;
+        _power++;
+        shootSlider.value = _power / 30f;
+    }
+
+    private void ResetShootSlider()
+    {
+        // Resets power, shoot slider and getting available for new freekick.
+        _power = 0;
+        shootSlider.value = 0;
+        _isUnavailableForNewFreekick = false;
+    }
+
+    private IEnumerator WaitForResetShootSlider()
+    {
+        // Waits seconds after shooting for avoid the possibility of the player accidentally skipping the shoot and taking a new freekick.
+        _isBallHit = true;
+        _isUnavailableForNewFreekick = true;
+        yield return new WaitForSecondsRealtime(4f);
+        SetInstructionPanels(false, false, true);
+        ResetShootSlider();
+    }
+
+    private void ShootToTargets()
+    {
+        // Shooting near and far targets.
+        if (ballDistanceText.gameObject.activeSelf) ballDistanceText.gameObject.SetActive(false);
+        SetInstructionPanels(false, false, false);
+        BallSoundPlayer.PlaySound(shootAudioSource, shootAudioClip, 1 + (2 - _power * 0.0666f));
+        StartCoroutine(ShootToNearTarget());
+    }
+
+    private IEnumerator ShootToNearTarget()
+    {
+        // Shooting near target for initial movement.
+        while ((ball.position - nearTarget.position).magnitude > 0.2f)
+        {
+            ball.position = Vector3.MoveTowards(ball.position, nearTarget.position, 0.1f + _power / 60f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        ShootToFarTarget();
+        yield return null;
+    }
+
+    private void ShootToFarTarget()
+    {
+        // Adding force if ball arrives the near target.
+        var shoot = (farTarget.position - ball.position).normalized;
+        ball.AddForce((shoot + new Vector3(0f, _power / 105f, 0f)) * _power / 2.4f, ForceMode.Impulse);
+    }
+}
---
+++ README.md
@@ -0,0 +1,94 @@
+# Unity-Football-Freekick-Game
+ 
+This project is the creation of a "football freekick mechanism" similar to the "FIFA 23" video game's freekick mechanism, in Unity 3D game engine by a software developer named Mert Usta.
+
+There are some demo gifs for my project : 
+
+- Freekick Demo :
+  
+![freekick demo](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/71546018-f8dc-4717-907a-c8c8a2d4a06f)
+
+
+- Penalty Demo :
+  
+![penalty demo](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/d49485f3-e008-464a-8672-0c0b062d23ac)
+
+
+- Angled Freekick Demo :
+  
+![angled freekick demo](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/cb51cdc2-8952-4522-ae8f-bc8522175e46)
+
+
+- Long Distance Freekick Demo :
+  
+![long distance freekick demo](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/bf1e172c-a8a2-406c-b1a2-06e6c240df4c)
+
+
+- Project's Sounds Demo :
+
+
+
+https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/e35e2472-fb2b-474d-b7bd-8d9b7b3e1128
+
+
+
+
+## What is freekick ?
+
+- Football "Freekick" is kicking action to ball in the field of play. Players could "shoot" or "pass" the ball to the target for taking an advantage.
+
+## FIFA 23 video game's Freekick System
+
+[![IMAGE ALT TEXT](http://img.youtube.com/vi/DDTItj3gOJM/0.jpg)](http://www.youtube.com/watch?v=DDTItj3gOJM "FIFA 23 Fantastic Free Kick Goal Zambia vs Japan PS5")
+
+## How to play?
+ 
+- In this game project; there are states of freekick mechanism : "freekick position selection", "shoot" and "new freekick". When shooting completed, player could take new freekick again.
+
+### "Freekick Position Selection"
+![freekick position selection](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/2e99e1d2-d1f7-41db-8914-5c88884fe769)
+
+
+- Player could change "ball's position" with using "W","A","S","D" keys and moves the ball position.
+
+- Player could set the finalized "ball's position" with pressing "Enter" key.
+
+### "Shoot"
+![shoot](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/46ae7828-1a83-4fb3-b23d-ed6148b2907e)
+
+
+- Player could set "ball hit point" with using "W","A","S","D" keys.
+
+- Player could change "ball kick angle" with using "Right Arrow" and "Left Arrow" keys.
+
+- Player could shoot with holding "Space" key. For adding more power to ball, holding "Space" button more longer. (Power bar is in Bottom-Right corner of canvas.)
+
+### "New Freekick"
+
+- When shooting completed, player could take a new freekick with pressing "Enter" key.
+
+### Additional Settings
+- Player could open/close the visibility of "Football Stadium" with pressing "F" key.
+
+![open or close the visibility Stadium](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/a8937ca5-ed33-4444-bb51-442e4ff79486)
+
+
+- Player could change the height of freekick camera with using "Mouse Scroll" input.
+
+![height of freekick camera](https://github.com/mertusta1996/Unity-Football-Freekick-Game/assets/70747383/5ceb467c-7026-45ae-84cd-806d36ae415f)
+
+
+## What were the subjects focused on?
+
+- Creating a game mechanism clearly playable. (In my opinion, "FIFA 23" has wonderful "freekick mechanism" and I played so much time in this, I wanted design and code a game module like this.)
+- Goal's "Net" is an important part of a football game. I used Unity's "Cloth" feature for it.
+- Easy implementation of UI and making canvas simple.
+- Adding sounds and handling the sound's "pitch" value with ball's "power" variable and "distance with camera". (For calculating and generating sounds everytime and using sounds more realistic)
+- Creating a trajectory line renderer for bezier lines.
+- Designed a curve movement of gameObject (for the ball's movement), with not a complicated programming solution.
+- Finding and using free 3D models, free textures, free sound effects for any usage. (Sources : sketchfab.com, textures.com, creazilla.com, pixabay.com, flaticon.com)
+- Thinking and understanding how a game module could work, then I designed and coded this game module with my own ways.
+
+## Author
+
+- Mert Usta
